07ef867b8b997aa50cb44357ada22c9745bddd8e
Guenther Schmidt <schmig68@gmail.com>
2023-02-28 11:38:00 +0000

    behaviour implementation

--- lib/ecto_multi_repo/proxy.ex
+++ lib/ecto_multi_repo/proxy.ex
@@ -85,7 +85,18 @@ defmodule EctoMultiRepo.Proxy do
   end
 
   @impl GenStateMachine
-  def init(%{timeout: timout, repo_module: repo_module} = arg) do
+  def init(arg) do
+    actions = [{:next_event, :internal, arg}]
+    {:ok, :connecting, nil, actions}
+  end
+
+  @impl GenStateMachine
+  def handle_event(
+        :internal,
+        %{timeout: timout, repo_module: repo_module} = arg,
+        :connecting,
+        nil
+      ) do
     params =
       arg
       |> Map.drop([:id, :timeout, :repo_module])
@@ -96,7 +107,7 @@ defmodule EctoMultiRepo.Proxy do
     {:ok, repo} = repo_module.start_link(params)
     repo_module.put_dynamic_repo(repo)
 
-    {:ok, :running, %{watchdog: watchdog, repo_module: repo_module}}
+    {:next_state, :running, %{watchdog: watchdog, repo_module: repo_module}}
   end
 
   @impl GenStateMachine
